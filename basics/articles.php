10fca17e5ab74532a0987f669611cc196294e21b
Clark Wilkins <simplexable@gmail.com>
2019-12-01 11:47:54 -0800

    1.01

--- basics/articles.php
+++ basics/articles.php
@@ -6,8 +6,9 @@
 	2019.12.01	Created. (1) (1.0)
 */
 
-$query = " SELECT * FROM help ORDER BY title; ";
-$getData = pg_query ( $connection, $query ) or $errors[3]['search.php'] = $query . "\n" . pg_last_error();
+$_SESSION['helpPlatform'] = $platform;
+$query = " SELECT * FROM help WHERE platform = {$platform} ORDER BY title; ";
+$getData = pg_query ( $connection, $query ) or $errors[4]['articles.php'] = $query . "\n" . pg_last_error();
 
 if ( $errors ) { return; }
 
